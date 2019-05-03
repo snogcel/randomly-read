312a6dd95ce4592066e25bf4d448c4657ded0611
Chris Garcia <chrisgarcia769@gmail.com>
2019-05-03 02:00:58 -0500

    RandomlyRead Page Layout

---
+++ client/src/components/RRLayout/RRComponent.js
@@ -0,0 +1,14 @@
+import React from 'react';
+import WordCard from './WordCard';
+
+class RandomlyRead extends React.Component {
+  render() {
+    return (
+      <div>
+        <WordCard />
+      </div>
+    );
+  }
+}
+
+export default RandomlyRead;
