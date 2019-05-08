9d0bcba10ca3c8f09e753fb554b1220f514b456d
ChrisYGarcia <ChrisGarcia769@gmail.com>
2019-05-08 17:45:20 -0500

    Removed this temporary test

--- client/src/index.js
+++ client/src/index.js
@@ -6,8 +6,6 @@ import { Provider } from 'react-redux';
 import store from './store';
 import AppContainer from './components/App/Container';
 import * as serviceWorker from './serviceWorker';
-import {addWord} from './actions/word'
-import {addVowel} from './actions/word'
 
 ReactDOM.render(
   <Provider store={store}>
@@ -16,12 +14,5 @@ ReactDOM.render(
   document.getElementById('root')
 );
 
-console.log(store.getState())
-const unsubscribe = store.subscribe(() => console.log(store.getState()))
-
-store.dispatch(addWord('Test Word'))
-store.dispatch(addVowel('o'))
-store.dispatch(addVowel('a'))
-unsubscribe()
 
 serviceWorker.unregister();
