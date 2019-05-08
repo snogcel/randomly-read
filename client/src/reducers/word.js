6b6b875a12b7f83fa2093e2d1aa301dd9cc27972
ChrisYGarcia <ChrisGarcia769@gmail.com>
2019-05-08 17:45:02 -0500

    Reducer to ADD and REMOVE words/vowels

--- client/src/reducers/word.js
+++ client/src/reducers/word.js
@@ -1,5 +1,7 @@
 import {ADD_VOWEL} from '../actions/word'
 import {ADD_WORD} from '../actions/word'
+import {REMOVE_VOWEL} from '../actions/word';
+import {REMOVE_WORD} from '../actions/word';
 
 const initialState = {
     text: [],
@@ -9,9 +11,13 @@ const initialState = {
 export default (state = initialState, action) => {
     switch (action.type) {
         case ADD_VOWEL:
-        return {...state, vowel: [...state.vowel, action.text]}
+        return {...state, vowel: state.vowel.concat([action.text])}
         case ADD_WORD:
         return {...state, text: action.text}
+        case REMOVE_VOWEL:
+        return {...state, vowel: state.vowel.filter(val => val !== action.text)}
+        case REMOVE_WORD:
+        return {...state, text: null}
         default: 
         return state;
     }
