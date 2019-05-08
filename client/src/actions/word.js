93b5f530a0edd6acd9b85d80b0905dc431786e5e
ChrisYGarcia <ChrisGarcia769@gmail.com>
2019-05-08 17:45:37 -0500

    Added new REMOVE actions

--- client/src/actions/word.js
+++ client/src/actions/word.js
@@ -1,5 +1,7 @@
 export const ADD_VOWEL = 'ADD_VOWEL'
 export const ADD_WORD = 'ADD_WORD'
+export const REMOVE_VOWEL = 'REMOVE_VOWEL';
+export const REMOVE_WORD = 'REMOVE_WORD';
 
 export function addVowel(text) {
     return {
@@ -10,5 +12,17 @@ export function addVowel(text) {
 export function addWord(text) {
     return {
         type: ADD_WORD, text
+    }
+}
+
+export function removeVowel(text) {
+    return {
+        type: REMOVE_VOWEL, text
+    }
+}
+
+export function removeWord() {
+    return {
+        type: REMOVE_WORD
     }
 }
