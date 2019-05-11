a46940cfaa8473c14e2c4a2c02cbf57c06876011
snogcel <ledniknoj@gmail.com>
2019-05-11 03:53:19 +0000

    implement timer functionality

--- client/src/components/RRLayout/RRComponent.js
+++ client/src/components/RRLayout/RRComponent.js
@@ -1,12 +1,109 @@
 import React from 'react';
 import WordCard from './WordCard';
+import Timer from './Timer';
 
 class RandomlyRead extends React.Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      mode: 'Word',
+      vowel: [],
+      consonant: [],
+      templates: [],
+      syllables: [],
+      location: 'initial',
+      limit: 1,
+      time: 0
+    };
+
+    this.timer = React.createRef();
+
+    this.timerHandler = this.timerHandler.bind(this);
+  }
+
+  timerHandler(options) {
+    let mode = this.state.mode;
+    let vowel = this.state.vowel;
+    let consonant = this.state.consonant;
+    let templates = this.state.templates;
+    let syllables = this.state.syllables;
+
+    let refresh = false;
+
+    function difference(lastProps, newProps) {
+      let newSet = new Set(newProps);
+      return lastProps.filter(function(x) { return !newSet.has(x); });
+    }
+
+
+    // Handle Consonants
+    let newConsonants = options.consonant;
+    let removedConsonants = difference(consonant, newConsonants);
+    if (removedConsonants.length === 0) refresh = true;
+    // this.consonantCheckboxes.current.unsetMany(removedConsonants);
+    // this.consonantCheckboxes.current.setMany(newConsonants);
+
+    // Handle Vowels
+    let newVowels = options.vowel;
+    let removedVowels = difference(vowel, newVowels);
+    // this.vowelCheckboxes.current.unsetMany(removedVowels);
+    // this.vowelCheckboxes.current.setMany(newVowels);
+
+    // Handle Syllables
+    // this.syllablesSelect.current.removeAll();
+    // this.syllablesSelect.current.setMany(options.syllables);
+
+    let limitText = this.state.limit;
+
+    if(!options.intermissionText) {
+
+      // Handle Word / Sentence Mode
+      // this.modeSelect.current.set(options.mode);
+
+    } else {
+
+      // Handle Intermission
+      // this.queryWindow.current.setMode('intermission', options.intermissionText);
+
+    }
+
+    mode = options.mode;
+    vowel = options.vowel;
+    consonant = options.consonant;
+    templates = options.templates;
+    syllables = options.syllables;
+
+    if (this.state.mode === "Intermission") refresh = false;
+
+    this.setState({
+      mode: mode,
+      vowel: vowel,
+      consonant: consonant,
+      templates: templates,
+      syllables: syllables,
+      limit: limitText,
+      time: Math.round((new Date()).getTime() / 1000)
+    });
+
+    if (refresh) {
+      console.log("Refetching...");
+      console.log(options);
+      // this.queryWindow.current.refreshQuery();
+    } else {
+      console.log("Fetching...");
+      console.log(options);
+    }
+
+  }
+
   render() {
     return (
       <div>
+
         <WordCard />
-        
+
+        <Timer ref={this.timer} action={this.timerHandler} />
+
       </div>
     );
   }
---
+++ client/src/components/RRLayout/RoutineBuilder.js
@@ -0,0 +1,173 @@
+import Blacklist from './Blacklist';
+
+const RoutineBuilder = function() {
+
+};
+
+RoutineBuilder.prototype._verifyBlacklist = function(vowel, consonant) {
+
+  let vowelBlacklist = Blacklist[vowel].consonants;
+
+  if (vowelBlacklist.indexOf(consonant) > -1) {
+
+    // console.log("Blocked Consonant: ", consonant);
+
+    return false;
+  }
+
+  return true;
+};
+
+RoutineBuilder.prototype._buildActionBase = function(exerciseConfig) {
+  let actionBase = {
+    mode: exerciseConfig.mode,
+    vowel: [],
+    consonant: [],
+    templates: exerciseConfig.templates,
+    syllables: exerciseConfig.syllables,
+    limit: exerciseConfig.limit
+  };
+
+  if (exerciseConfig.intermissionText) actionBase.intermissionText = exerciseConfig.intermissionText;
+
+  return actionBase;
+};
+
+RoutineBuilder.prototype._buildCheckpoints = function(exerciseConfig) {
+  let checkpoints = [];
+  let i = 0;
+
+  while (i < exerciseConfig.duration) {
+    checkpoints.push(i);
+    i = i + exerciseConfig.rangeVal;
+  }
+
+  return checkpoints;
+};
+
+RoutineBuilder.prototype.buildIntermission = function(exerciseConfig) {
+  let routine = new Map();
+
+  // Generate Action Base
+  let actionBase = this._buildActionBase(exerciseConfig);
+
+  // Set Default Action
+  let action = JSON.parse(JSON.stringify(actionBase));
+
+  routine.set(0, action);
+
+  return routine;
+};
+
+RoutineBuilder.prototype.buildRandomly = function(exerciseConfig) {
+  let routine = new Map();
+
+  // Generate Action Base
+  let actionBase = this._buildActionBase(exerciseConfig);
+
+  // Define Exercise Checkpoints
+  let checkpoints = this._buildCheckpoints(exerciseConfig);
+
+  // Iterate Through Checkpoints
+  let j = 0; // vowel iterator
+  let k = 0; // consonant iterator
+
+  for (let i = 0; i < checkpoints.length; i++) {
+
+    // Set Default Action
+    let action = JSON.parse(JSON.stringify(actionBase));
+
+    // Generate Random Number
+    let rand = Math.floor(Math.random() * (exerciseConfig.consonants[k].length));
+
+    let verified = false;
+    let consonant = exerciseConfig.consonants[k][rand]; // Use Random Number
+
+    verified = this._verifyBlacklist(exerciseConfig.vowels[j], consonant); // set and verify initial matched word
+
+    while (!verified) {
+      rand = Math.floor(Math.random() * (exerciseConfig.consonants.length + 1));
+      consonant = exerciseConfig.consonants[k][rand];
+      verified = this._verifyBlacklist(exerciseConfig.vowels[j], consonant);
+      if (verified) console.log('Word replaced with: ' + consonant);
+    }
+
+    // Set Parameters
+    action.consonant.unshift(consonant); // add consonant to array
+    action.vowel.unshift(exerciseConfig.vowels[j]); // add vowel to array
+
+    // Iterate Vowels and Consonants
+    if (k < exerciseConfig.consonants[k].length - 1) {
+
+      // Logic Applied if Random Selection is active
+      if (k < (exerciseConfig.repetitions - 1)) {
+
+        k++; // increment consonant
+
+      } else {
+
+        k = 0; // reset consonant
+
+        // Iterate Vowels
+        if (j < exerciseConfig.vowels.length - 1) { j++; } else { j = 0; }
+
+      }
+
+    } else {
+
+      k = 0; // reset consonant
+
+      // Iterate Vowels
+      if (j < exerciseConfig.vowels.length - 1) { j++; } else { j = 0; }
+    }
+
+    routine.set(checkpoints[i], action);
+  }
+
+  return routine;
+
+};
+
+RoutineBuilder.prototype.build = function(exerciseConfig) {
+  let routine = new Map();
+
+  // Generate Action Base
+  let actionBase = this._buildActionBase(exerciseConfig);
+
+  // Define Exercise Checkpoints
+  let checkpoints = this._buildCheckpoints(exerciseConfig);
+
+  // Iterate Through Checkpoints
+  let j = 0; // vowel iterator
+  let k = 0; // consonant iterator
+
+  for (let i = 0; i < checkpoints.length; i++) {
+
+    // Set Default Action
+    let action = JSON.parse(JSON.stringify(actionBase));
+
+    // Set Parameters
+    action.consonant = exerciseConfig.consonants[k]; // add all consonants to array
+    action.vowel.unshift(exerciseConfig.vowels[j]); // add vowel to array
+
+    if (k < (exerciseConfig.repetitions - 1)) {
+
+      k++; // increment consonant counter
+
+    } else {
+
+      k = 0; // reset consonant
+
+      // Iterate Vowels
+      if (j < exerciseConfig.vowels.length - 1) { j++; } else { j = 0; }
+
+    }
+
+    routine.set(checkpoints[i], action);
+  }
+
+  return routine;
+
+};
+
+export default RoutineBuilder;
