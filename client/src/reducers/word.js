b43c049cc536c137090f777974bb3da132f6d05d
ChrisYGarcia <ChrisGarcia769@gmail.com>
2019-05-14 01:24:00 -0500

    Updated router to prevent duplicates and work with strings and arrays. Also, added new actions to reducer for consonants, syllables, limit and mode

--- client/src/reducers/word.js
+++ client/src/reducers/word.js
@@ -2,22 +2,38 @@ import {ADD_VOWEL} from '../actions/word'
 import {ADD_WORD} from '../actions/word'
 import {REMOVE_VOWEL} from '../actions/word';
 import {REMOVE_WORD} from '../actions/word';
+import {ADD_CONSONANT} from '../actions/word';
+import {SET_LIMIT} from '../actions/word';
+import {SET_MODE} from '../actions/word';
+import {ADD_SYLLABLES} from '../actions/word';
 
 const initialState = {
     text: [],
-    vowel: []
+    vowel: null,
+    consonant: ["F"],
+    syllables: [1],
+    limit: 1,
+    mode: "Word"
 }
 
 export default (state = initialState, action) => {
     switch (action.type) {
         case ADD_VOWEL:
-        return {...state, vowel: state.vowel.concat([action.text])}
+        return {...state, vowel: !Array.isArray(state.vowel) ? action.text : [...new Set([...state.vowel].concat(action.text))]}//vowel: state.vowel.concat([action.text])}
         case ADD_WORD:
         return {...state, text: action.text}
         case REMOVE_VOWEL:
-        return {...state, vowel: state.vowel.filter(val => val !== action.text)}
+        return {...state, vowel: !Array.isArray(state.vowel) ? null : state.vowel.filter(val => val !== action.text)}
         case REMOVE_WORD:
         return {...state, text: null}
+        case ADD_CONSONANT:
+        return {...state, consonant: action.text}//state.consonant.concat([action.text])}
+        case ADD_SYLLABLES:
+        return {...state, syllables: parseInt(action.integer)}//state.syllables.concat([parseInt(action.integer)])}
+        case SET_LIMIT:
+        return {...state, limit: action.text}
+        case SET_MODE:
+        return {...state, mode: action.text}
         default: 
         return state;
     }
