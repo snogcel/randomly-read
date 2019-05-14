b1f791795ceed831b24022f780be71266163e823
ChrisYGarcia <ChrisGarcia769@gmail.com>
2019-05-14 01:21:24 -0500

    Timer Reducer

---
+++ client/src/components/RRLayout/TimerContainer.js
@@ -0,0 +1,42 @@
+import { connect } from 'react-redux';
+import {addVowel} from '../../actions/word'
+import {addConsonant} from '../../actions/word'
+import {addSyllables} from '../../actions/word'
+import {setLimit} from '../../actions/word'
+import {setMode} from '../../actions/word'
+import Timer from './RRComponent';
+
+const mapStateToProps = state => ({
+    mode: state.mode,
+    vowel: state.vowel,
+    consonant: state.consonant,
+    syllables: state.syllables,
+    limit: state.limit
+
+  });
+
+  const mapDispatchToProps = dispatch => ({
+    addVowel: (vowel) => {
+      dispatch(addVowel(vowel))
+    },
+    addConsonant: (consonant) => {
+      dispatch(addConsonant(consonant))
+    },
+    addSyllables: (syllables) => {
+        dispatch(addSyllables(syllables))
+      },
+    setLimit: (limit) => {
+      dispatch(setLimit(limit))
+    },
+    setMode: (mode) => {
+    dispatch(setMode(mode))
+  }
+  
+  });
+
+  const TimerContainer = connect(
+    mapStateToProps,
+    mapDispatchToProps
+  )(Timer);
+  
+  export default TimerContainer;
