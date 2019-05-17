a49f4c1cc20cefa96f9613f4da38af06b7154a50
ChrisYGarcia <ChrisGarcia769@gmail.com>
2019-05-17 04:11:46 -0500

    Added vowel Checkboxes in a seperate file in order to map over them and render them with dynamic toggling

---
+++ client/src/components/RRLayout/VowelCheckboxes.js
@@ -0,0 +1,95 @@
+const checkboxes = [
+    {
+        name: 'AA',
+        key: 'vowel_AA',
+        label: 'ɑ',
+  
+    },
+    {
+        name: 'AE',
+        key: 'vowel_AE',
+        label: 'æ',
+
+    },
+    {
+        name: 'AH',
+        key: 'vowel_AH',
+        label: 'ʌ',
+ 
+    },
+    {
+        name: 'AO',
+        key: 'vowel_AO',
+        label: 'ɔ',
+ 
+    },
+    {
+        name: 'AW',
+        key: 'vowel_AW',
+        label: 'aʊ',
+
+    },
+    {
+        name: 'AY',
+        key: 'vowel_AY',
+        label: 'aɪ',
+
+    },
+    {
+        name: 'EH',
+        key: 'vowel_EH',
+        label: 'ɛ',
+
+    },
+    {
+        name: 'ER',
+        key: 'vowel_ER',
+        label: 'ɝ',
+
+    },
+    {
+        name: 'EY',
+        key: 'vowel_EY',
+        label: 'eɪ',
+
+    },
+    {
+        name: 'IH',
+        key: 'vowel_IH',
+        label: 'ɪ',
+
+    },
+    {
+        name: 'IY',
+        key: 'vowel_IY',
+        label: 'i',
+
+    },
+    {
+        name: 'OW',
+        key: 'vowel_OW',
+        label: 'oʊ',
+
+    },
+    {
+        name: 'OY',
+        key: 'vowel_OY',
+        label: 'ɔɪ',
+
+    },
+    {
+        name: 'UH',
+        key: 'vowel_UH',
+        label: 'ʊ',
+
+    },
+    {
+        name: 'UW',
+        key: 'vowel_UW',
+        label: 'u',
+       
+    },
+   
+];
+
+export default checkboxes;
