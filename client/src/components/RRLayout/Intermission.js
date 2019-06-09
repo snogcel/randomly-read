357cc8fa4dbc225f3bef04136118393ed3d29b5f
ChrisYGarcia <ChrisGarcia769@gmail.com>
2019-06-08 22:06:47 -0500

    Refactored to function components

--- client/src/components/RRLayout/Intermission.js
+++ client/src/components/RRLayout/Intermission.js
@@ -2,15 +2,8 @@ import React from 'react'
 import Typography from '@material-ui/core/Typography';
 
 
-class Intermission extends React.Component {
-
-    render() {
-    return (
-    <Typography style={{color: this.props.dark === true ? 'white' : 'black'}} variant='h2' component='h2' align='center'>{this.props.intermissionText}</Typography>
-    )
-    }
-
-}
-
-
+const Intermission = props => (
+    <Typography style={{color: props.dark === true ? 'white' : 'black'}} variant='h2' component='h2' align='center'>{props.intermissionText}</Typography>
+     );
+    
 export default Intermission;
