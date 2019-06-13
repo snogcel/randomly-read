08a669ca2d1ac93bae168e59f3707b07c2c8dfe0
snogcel <ledniknoj@gmail.com>
2019-06-13 17:33:53 +0000

    fix error 401

--- server/controllers/interactions.js
+++ server/controllers/interactions.js
@@ -1,6 +1,14 @@
+const { body, validationResult } = require('express-validator/check');
 const Interaction = require('../models/interaction');
 
 exports.create = async (req, res, next) => {
+
+    const result = validationResult(req);
+    if (!result.isEmpty()) {
+      const errors = result.array({ onlyFirstError: true });
+      return res.status(422).json({ errors });
+    }
+
     try {
       const { setting, audience, intention, ease } = req.body;
       const author = req.user.id;
@@ -15,10 +23,10 @@ exports.create = async (req, res, next) => {
     } catch (err) {
       next(err);
     }
+
   };
 
   exports.list = async (req, res) => {
     const interactions =  Interaction.find();
     res.json(interactions);
   };
-  
--- server/routes.js
+++ server/routes.js
@@ -5,7 +5,7 @@ const users = require('./controllers/users');
 const posts = require('./controllers/posts');
 const interactions = require('./controllers/interactions');
 const comments = require('./controllers/comments');
-const { jwtAuth, postAuth, commentAuth } = require('./auth');
+const { jwtAuth, postAuth, commentAuth, interactionAuth } = require('./auth');
 const router = require('express').Router();
 
 router.post('/login', users.validate(), users.login);
@@ -24,7 +24,7 @@ router.get('/user/:user', posts.listByUser);
 router.param('comment', comments.load);
 router.post('/post/:post', [jwtAuth, comments.validate], comments.create);
 router.delete('/post/:post/:comment', [jwtAuth, commentAuth], comments.destroy);
-router.post('/interactions', [jwtAuth], interactions.create);
+router.post('/interactions', [jwtAuth, interactionAuth], interactions.create);
 router.get('/interactions', interactions.list);
 
 module.exports = app => {
