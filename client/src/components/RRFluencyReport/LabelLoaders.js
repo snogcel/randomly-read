a7f0f729a47f1d38f1f4de2a11ee46809ca868dc
ChrisYGarcia <ChrisGarcia769@gmail.com>
2019-06-19 03:50:37 -0500

    LabelLoaders outputs label values based off the input data value of the diffrent field values

---
+++ client/src/components/RRFluencyReport/LabelLoaders.js
@@ -0,0 +1,69 @@
+export const settingLabelLoader = (option) => {
+    switch (option) {
+        case "1":
+        return "Speaking at Work or School"
+        case "2":
+        return "Speaking on the Phone"
+        case "3":
+        return "Presenting a Topic"
+        case "4":
+        return "Attending a Social Event"
+        case "5":
+        return "Relaxing with Friends"
+        case "6":
+        return "Relaxing at Home"
+        default: 
+        return option;
+    }
+}
+
+export const audienceLabelLoader = (option) => {
+    switch (option) {
+        case "1":
+        return "Parent"
+        case "2":
+        return "Family"
+        case "3":
+        return "Significant Other"
+        case "4":
+        return "Friend"
+        case "5":
+        return "Coworker / Classmate"
+        case "6":
+        return "Authority Figure"
+        case "7":
+        return "Service Worker"
+        case "8":
+        return "No Relationship"
+        default: 
+        return option;
+    }
+}
+
+export const intentionLabelLoader = (option) => {
+    switch (option) {
+        case "1":
+        return "I did not remember or use"
+        case "5":
+        return "I remembered but did not use"
+        case "10":
+        return "I remembered and used"
+        default: 
+        return option;
+    }
+}
+
+export const easeLabelLoader = (option) => {
+    switch (option) {
+        case "1":
+        return "Speech was difficult"
+        case "4":
+        return "Speech was less difficult"
+        case "7":
+        return "Speech was easier"
+        case "10":
+        return "Speech was easy"
+        default: 
+        return option;
+    }
+}
