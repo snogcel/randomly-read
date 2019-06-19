bd02987c04b7175d3251e543f3ba595d2a26a773
ChrisYGarcia <ChrisGarcia769@gmail.com>
2019-06-19 03:44:05 -0500

    added "formData" to the global Redux Store. This will allow the 6 forms and 6 data tables to communicate among the 6 form data group

--- client/src/actions/formData.js
+++ client/src/actions/formData.js
@@ -1,10 +1,136 @@
-export const ADD_FORM_DATA = 'ADD_FORM_DATA';
+export const LOAD_SETTING_1_FORM_DATA = 'LOAD_SETTING_1_FORM_DATA';
+export const ADD_SETTING_1_FORM_DATA = 'ADD_SETTING_1_FORM_DATA';
+export const DELETE_SETTING_1_FORM_DATA = 'DELETE_SETTING_1_FORM_DATA';
+
+export const LOAD_SETTING_2_FORM_DATA = 'LOAD_SETTING_2_FORM_DATA';
+export const ADD_SETTING_2_FORM_DATA = 'ADD_SETTING_2_FORM_DATA';
+export const DELETE_SETTING_2_FORM_DATA = 'DELETE_SETTING_2_FORM_DATA';
+
+export const LOAD_SETTING_3_FORM_DATA = 'LOAD_SETTING_3_FORM_DATA';
+export const ADD_SETTING_3_FORM_DATA = 'ADD_SETTING_3_FORM_DATA';
+export const DELETE_SETTING_3_FORM_DATA = 'DELETE_SETTING_3_FORM_DATA';
+
+export const LOAD_SETTING_4_FORM_DATA = 'LOAD_SETTING_4_FORM_DATA';
+export const ADD_SETTING_4_FORM_DATA = 'ADD_SETTING_4_FORM_DATA';
+export const DELETE_SETTING_4_FORM_DATA = 'DELETE_SETTING_4_FORM_DATA';
+
+export const LOAD_SETTING_5_FORM_DATA = 'LOAD_SETTING_5_FORM_DATA';
+export const ADD_SETTING_5_FORM_DATA = 'ADD_SETTING_5_FORM_DATA';
+export const DELETE_SETTING_5_FORM_DATA = 'DELETE_SETTING_5_FORM_DATA';
+
+export const LOAD_SETTING_6_FORM_DATA = 'LOAD_SETTING_6_FORM_DATA';
+export const ADD_SETTING_6_FORM_DATA = 'ADD_SETTING_6_FORM_DATA';
+export const DELETE_SETTING_6_FORM_DATA = 'DELETE_SETTING_6_FORM_DATA';
+
 export const RESET_FORM_DATA = 'RESET_FORM_DATA';
 export const ADD_INITIAL_FORM_DATA = 'ADD_INITIAL_FORM_DATA';
 
-export function addFormData(text) {
+export function loadSetting1FormData(text) {
     return {
-        type: ADD_FORM_DATA, text
+        type: LOAD_SETTING_1_FORM_DATA, text
+    }
+}
+
+export function addSetting1FormData(text) {
+    return {
+        type: ADD_SETTING_1_FORM_DATA, text
+    }
+}
+
+export function deleteSetting1FormData(text) {
+    return {
+        type: DELETE_SETTING_1_FORM_DATA, text
+    }
+}
+
+export function loadSetting2FormData(text) {
+    return {
+        type: LOAD_SETTING_2_FORM_DATA, text
+    }
+}
+
+
+export function addSetting2FormData(text) {
+    return {
+        type: ADD_SETTING_2_FORM_DATA, text
+    }
+}
+
+export function deleteSetting2FormData(text) {
+    return {
+        type: DELETE_SETTING_2_FORM_DATA, text
+    }
+}
+
+export function loadSetting3FormData(text) {
+    return {
+        type: LOAD_SETTING_3_FORM_DATA, text
+    }
+}
+
+export function addSetting3FormData(text) {
+    return {
+        type: ADD_SETTING_3_FORM_DATA, text
+    }
+}
+
+export function deleteSetting3FormData(text) {
+    return {
+        type: DELETE_SETTING_3_FORM_DATA, text
+    }
+}
+
+export function loadSetting4FormData(text) {
+    return {
+        type: LOAD_SETTING_4_FORM_DATA, text
+    }
+}
+
+export function addSetting4FormData(text) {
+    return {
+        type: ADD_SETTING_4_FORM_DATA, text
+    }
+}
+
+export function deleteSetting4FormData(text) {
+    return {
+        type: DELETE_SETTING_4_FORM_DATA, text
+    }
+}
+
+export function loadSetting5FormData(text) {
+    return {
+        type: LOAD_SETTING_5_FORM_DATA, text
+    }
+}
+
+export function addSetting5FormData(text) {
+    return {
+        type: ADD_SETTING_5_FORM_DATA, text
+    }
+}
+
+export function deleteSetting5FormData(text) {
+    return {
+        type: DELETE_SETTING_5_FORM_DATA, text
+    }
+}
+
+export function loadSetting6FormData(text) {
+    return {
+        type: LOAD_SETTING_6_FORM_DATA, text
+    }
+}
+
+export function addSetting6FormData(text) {
+    return {
+        type: ADD_SETTING_6_FORM_DATA, text
+    }
+}
+
+export function deleteSetting6FormData(text) {
+    return {
+        type: DELETE_SETTING_6_FORM_DATA, text
     }
 }
 
--- client/src/reducers/formData.js
+++ client/src/reducers/formData.js
@@ -1,19 +1,74 @@
-import {ADD_FORM_DATA} from '../actions/formData'
+import {ADD_SETTING_1_FORM_DATA} from '../actions/formData'
+import {DELETE_SETTING_1_FORM_DATA} from '../actions/formData'
+import {ADD_SETTING_2_FORM_DATA} from '../actions/formData'
+import {DELETE_SETTING_2_FORM_DATA} from '../actions/formData'
+import {ADD_SETTING_3_FORM_DATA} from '../actions/formData'
+import {DELETE_SETTING_3_FORM_DATA} from '../actions/formData'
+import {ADD_SETTING_4_FORM_DATA} from '../actions/formData'
+import {DELETE_SETTING_4_FORM_DATA} from '../actions/formData'
+import {ADD_SETTING_5_FORM_DATA} from '../actions/formData'
+import {DELETE_SETTING_5_FORM_DATA} from '../actions/formData'
+import {ADD_SETTING_6_FORM_DATA} from '../actions/formData'
+import {DELETE_SETTING_6_FORM_DATA} from '../actions/formData'
 import {RESET_FORM_DATA} from '../actions/formData'
-import {ADD_INITIAL_FORM_DATA} from '../actions/formData'
+import {LOAD_SETTING_1_FORM_DATA} from '../actions/formData'
+import {LOAD_SETTING_2_FORM_DATA} from '../actions/formData'
+import {LOAD_SETTING_3_FORM_DATA} from '../actions/formData'
+import {LOAD_SETTING_4_FORM_DATA} from '../actions/formData'
+import {LOAD_SETTING_5_FORM_DATA} from '../actions/formData'
+import {LOAD_SETTING_6_FORM_DATA} from '../actions/formData'
 
 const initialState = {
-   formData: []
+
+    setting1: [],
+    setting2: [],
+    setting3: [],
+    setting4: [],
+    setting5: [],
+    setting6: []
+
 }
 
 export default (state = initialState, action) => {
     switch (action.type) {
-        case ADD_INITIAL_FORM_DATA:
-        return {...state, formData: action.text}
-        case ADD_FORM_DATA:
-        return {...state, formData: state.formData.concat([action.text])}//vowel: state.vowel.concat([action.text])}
+        case LOAD_SETTING_1_FORM_DATA:
+        return {...state, setting1: action.text}
+        case ADD_SETTING_1_FORM_DATA:
+        return {...state, setting1: [...state.setting1, action.text]}//vowel: state.vowel.concat([action.text])}
+        case DELETE_SETTING_1_FORM_DATA:
+        return {...state, setting1: action.text} 
+        case LOAD_SETTING_2_FORM_DATA:
+        return {...state, setting2: action.text}
+        case ADD_SETTING_2_FORM_DATA:
+        return {...state, setting2: [...state.setting2, action.text]}//vowel: state.vowel.concat([action.text])}
+        case DELETE_SETTING_2_FORM_DATA:
+        return {...state, setting2: action.text} 
+        case LOAD_SETTING_3_FORM_DATA:
+        return {...state, setting3: action.text}
+        case ADD_SETTING_3_FORM_DATA:
+        return {...state, setting3: [...state.setting3, action.text]}//vowel: state.vowel.concat([action.text])}
+        case DELETE_SETTING_3_FORM_DATA:
+        return {...state, setting3: action.text} 
+        case LOAD_SETTING_4_FORM_DATA:
+        return {...state, setting4: action.text}
+        case ADD_SETTING_4_FORM_DATA:
+        return {...state, setting4: [...state.setting4, action.text]}//vowel: state.vowel.concat([action.text])}
+        case DELETE_SETTING_4_FORM_DATA:
+        return {...state, setting4: action.text} 
+        case LOAD_SETTING_5_FORM_DATA:
+        return {...state, setting5: action.text}
+        case ADD_SETTING_5_FORM_DATA:
+        return {...state, setting5: [...state.setting5, action.text]}//vowel: state.vowel.concat([action.text])}
+        case DELETE_SETTING_5_FORM_DATA:
+        return {...state, setting5: action.text} 
+        case LOAD_SETTING_6_FORM_DATA:
+        return {...state, setting6: action.text}
+        case ADD_SETTING_6_FORM_DATA:
+        return {...state, setting6: [...state.setting6, action.text]}//vowel: state.vowel.concat([action.text])}
+        case DELETE_SETTING_6_FORM_DATA:
+        return {...state, setting6: action.text} 
         case RESET_FORM_DATA:
-        return {...state, formData: []};
+        return {...state, setting1: [], setting2: [], setting3: [], setting4: [], setting5: [], setting6: []};
         default: 
         return state;
     }
