36a2b191e91359c894197ee80f130ee8f61db747
snogcel <ledniknoj@gmail.com>
2019-06-22 08:07:39 +0000

    adjust button colors

--- client/src/themeHandler.js
+++ client/src/themeHandler.js
@@ -9,13 +9,12 @@ const MuiTheme = createMuiTheme({
   palette: {
     primary: {
       // light: will be calculated from palette.primary.main,
-      main: '#ff4400',
+      main: '#2f8eed',
       // dark: will be calculated from palette.primary.main,
       // contrastText: will be calculated to contrast with palette.primary.main
     },
     secondary: {
-      light: '#0066ff',
-      main: '#0044ff',
+      main: '#14197E',
       // dark: will be calculated from palette.secondary.main,
       contrastText: '#ffcc00',
     }
