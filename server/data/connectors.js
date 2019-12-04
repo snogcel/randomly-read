e00af18b85366176b2bb56b1a639da91f292e606
snogcel <ledniknoj@gmail.com>
2019-12-04 20:33:25 +0000

    update database name

--- server/data/connectors.js
+++ server/data/connectors.js
@@ -1,6 +1,6 @@
 const Sequelize = require('sequelize');
 
-const database = new Sequelize('words','stuttered','E]]q2u6xGPUvy[\?', {
+const database = new Sequelize('worddb','stuttered','E]]q2u6xGPUvy[\?', {
     host: 'localhost',
     port: 3306,
     dialect: 'mysql',
