4150d87acdf1111049a36e9494ef95ad90b84617
snogcel <ledniknoj@gmail.com>
2021-03-29 02:38:22 +0000

    use state (instead of localstorage) to fetch token)

--- client/src/components/App/Component.js
+++ client/src/components/App/Component.js
@@ -6,69 +6,79 @@ import { MuiThemeProvider } from '@material-ui/core/styles'
 import theme from '../../theme'; // TODO - remove
 import { MuiTheme } from '../../themeHandler';
 import history from '../../util/history';
+
 import GlobalStyle from '../../globalStyle';
 import AppBarContainer from '../AppBar/Container';
 import HeaderContainer from '../Header/Container';
 import ErrorNotificationContainer from '../ErrorNotification/Container';
+
 import LoginFormContainer from '../LoginForm/Container';
 import SignupFormContainer from '../SignupForm/Container';
 import CreatePostFormContainer from '../CreatePostForm/Container';
+
 import Home from '../Home';
+
 import ApolloClient from 'apollo-client';
 
 import { HttpLink } from 'apollo-link-http';
 import { ApolloLink } from 'apollo-link';
 import { InMemoryCache as Cache } from 'apollo-cache-inmemory';
-
 import { ApolloProvider } from 'react-apollo';
+
+
+
+
 import RRHomeContainer from '../RRLayout/RRHomeContainer'
+import RRHomeDevContainer from '../RRLayout/RRHomeContainerDev'
 import FluencyReport from '../RRFluencyReport/FluencyReport'
-import Interactions from '../Interactions/InteractionsHomeContainer';
+// import Interactions from '../Interactions/InteractionsHomeContainer';
 import RoutineBuilder from '../RoutineBuilder/RoutineBuilderContainer';
 import Administration from '../Administration/Container';
 import UserProfile from '../UserProfile/Container';
 import UserHome from '../UserHome/Container';
 
-const AuthLink = (operation, next) => {
-  const token = localStorage.getItem('token');
+const App = props => {
 
-  operation.setContext(context => ({
-    ...context,
-    headers: {
-      ...context.headers,
-      Authorization: `Bearer ${token}`,
-    },
-  }));
+  const {user, token} = props;
 
-  return next(operation);
-};
+  const AuthLink = (operation, next) => {
 
-const baseUrl =
-  process.env.NODE_ENV === 'development'
-    ? 'http://dev.snogcel.com:8080/graphql'
-    : `https://api.stuttered.net/graphql`;
+    operation.setContext(context => ({
+      ...context,
+      headers: {
+        ...context.headers,
+        Authorization: `Bearer ${token}`,
+      },
+    }));
 
-const link = ApolloLink.from([
-  AuthLink,
-  new HttpLink({ uri: baseUrl }),
-]);
+    return next(operation);
+  };
 
-const client = new ApolloClient({
-  link,
-  cache: new Cache().restore({}),
-});
+  const baseUrl =
+    process.env.NODE_ENV === 'development'
+      ? 'http://dev.snogcel.com:8080/graphql'
+      : `https://api.stuttered.net/graphql`;
 
-// <Route component={HeaderContainer} />
+  const link = ApolloLink.from([
+    AuthLink,
+    new HttpLink({ uri: baseUrl }),
+  ]);
 
-ReactGA.initialize('UA-109552567-1');
-ReactGA.pageview(window.location.pathname + window.location.search);
+  const client = new ApolloClient({
+    link,
+    cache: new Cache().restore({}),
+  });
 
-const App = props => (
+  // Google Analytics
+  ReactGA.initialize('UA-109552567-1');
+  ReactGA.pageview(window.location.pathname + window.location.search);
+
+  return (
   <MuiThemeProvider theme={MuiTheme}>
     <ApolloProvider client={client}>
       <ThemeProvider theme={theme(props.dark)}>
         <Router history={history}>
-          <>
+          <div>
             <GlobalStyle />
             <Route component={AppBarContainer} />
             <Route component={ErrorNotificationContainer} />
@@ -77,18 +87,18 @@ const App = props => (
               <Route path='/signup' component={SignupFormContainer} />
               <Route path='/createpost' component={CreatePostFormContainer} />
               <Route path='/RandomlyRead' component={RRHomeContainer} />
+              <Route path='/RandomlyReadDev' component={RRHomeDevContainer} />
               <Route path='/FluencyReport' component={FluencyReport} />
-              <Route path='/FocusWords' component={Interactions} />
               <Route path='/RoutineBuilder' component={RoutineBuilder} />
               <Route path='/Administration' component={Administration} />
               <Route path='/Profile' component={UserProfile} />
               <Route path='/' component={RRHomeContainer} />
             </Switch>
-          </>
+          </div>
         </Router>
       </ThemeProvider>
     </ApolloProvider>
   </MuiThemeProvider>
-);
+)};
 
 export default App;
--- client/src/components/App/Container.js
+++ client/src/components/App/Container.js
@@ -1,8 +1,20 @@
 import { connect } from 'react-redux';
+import { compose } from 'redux';
 import App from './Component';
+import withAuth from '../../util/withAuth';
 
+const enhance = compose(
+  withAuth
+);
+
+const AppContainer = enhance(App);
+
+export default AppContainer;
+
+/*
 const mapStateToProps = state => ({ dark: state.theme.dark });
 
 const AppContainer = connect(mapStateToProps)(App);
 
 export default AppContainer;
+*/
