c9379586114c2db05da29bd8b626dc0ac25f9b80
snogcel <ledniknoj@gmail.com>
2021-05-31 03:05:31 +0000

    fix jslint warning

--- client/src/components/RandomlyRead/Timer.js
+++ client/src/components/RandomlyRead/Timer.js
@@ -4,7 +4,6 @@ import RoutineBuilder from '../RRLayout/RoutineBuilder';
 import Box from '@material-ui/core/Box';
 import Grid from '@material-ui/core/Grid';
 
-import { Typography } from '@material-ui/core';
 import { withStyles } from "@material-ui/core/styles";
 import IconButton from '@material-ui/core/IconButton';
 
