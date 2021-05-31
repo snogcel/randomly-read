4a867dae44fb932d37c604b3e5b8164b1b6c09a3
snogcel <ledniknoj@gmail.com>
2021-05-31 02:59:15 +0000

    adjust server database variables

--- server/config.js
+++ server/config.js
@@ -1,7 +1,7 @@
 module.exports = {
   port: 8080,
   db: {
-    prod: process.env.DATABASE_URL || 'mongodb://localhost/reddit',
+    prod: process.env.DATABASE_URL || 'mongodb://localhost/production',
     test: 'mongodb://localhost/reddit_test',
     options: {
       useNewUrlParser: true,
